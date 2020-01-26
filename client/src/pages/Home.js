65e5e6829c166000176e059d89a54a7c15bccb9c
Oumar96 <oumarba221296@hotmail.fr>
2020-01-26 02:50:51 -0500

    Calling DB

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Navbar from '../components/Navbar/Navbar';
 import {Nav} from 'react-bootstrap';
 import { Card, CardBody, Divider } from '../components/Card/Card';
@@ -10,16 +10,54 @@ import Subtitle from '../components/Subtitle/Subtitle';
 import Header from '../components/Header/Header';
 import Footer from '../components/Footer/Footer';
 
-import cards from '../utils/tools';
+import {getAssociations,getProjects} from '../utils/tools';
 
 import '../App.css';
 
 
-
 function Home(){
+    const [associations, setAssociations] = useState('Not available yet');
+    const [projects, setProjects] = useState('Not available yet');
+	const [isLoading, setIsLoading] = useState(false);
+    useEffect(() => {
+
+        async function fetchData() {
+            setIsLoading(true);
+            await fetch('http://localhost:4000/', {
+                method: 'POST',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify({
+                    query: `{
+                        myAssociations(user_id:"1"){
+                            association_name,
+                            description
+                        }
+                        volunteers(user_id:"1"){
+                            event_id,
+                            event_name,
+                            description,
+                            total_volunteers,
+                            activity_types
+                          }
+                    }`
+                })
+            })
+                .then((res) => res.json())
+                .then((res) => {
+                    (res.data.myAssociations && res.data.myAssociations.length !== 0) && setAssociations(res.data.myAssociations);
+                    (res.data.volunteers && res.data.volunteers.length !== 0) && setProjects(res.data.volunteers);
+                    setIsLoading(false);
+                });
+            }
+            fetchData();
+        }, [associations]);
+
+    const associationsList = associations;
+    const projectsList = projects;
+    // console.log(associationsList);
     const cardsNew = ["Hi", "sdfsdf","sdfsd","dsfsdf"];
-    const cardsComponents = cards(cardsNew);
-    console.log(cardsComponents);
+    const associationsComponents = getAssociations(associationsList);
+    const projectComponents = getProjects(projectsList);
     return(
         <div>
             <Header/>
@@ -29,7 +67,7 @@ function Home(){
                     className="justify-content-center"
                     style={{ padding: '5px 25px' }}
                 >
-                    {cardsComponents}
+                    {associationsComponents}
                 </Row>
                 <p><Nav.Link href='/Association'>I volunteer</Nav.Link></p>
                 <Subtitle text="Projects" />
@@ -37,7 +75,7 @@ function Home(){
                     className="justify-content-center"
                     style={{ padding: '5px 25px' }}
                 >
-                    {cardsComponents}
+                    {projectComponents}
                 </Row>
                 <p><Nav.Link href='/Project'>I volunteer</Nav.Link></p>
 
--- client/src/utils/tools.js
+++ client/src/utils/tools.js
@@ -3,22 +3,26 @@ import { Card, CardBody, Divider } from '../components/Card/Card';
 import { Row, Col, Card as RBCard } from 'react-bootstrap/';
 import {CardImg} from 'reactstrap';
 
-function cards(Object){
+function getAssociations(Object){
+    console.log(Object)
+    console.log(Object.length)
+    var length = Object.length;
     var i = 0;
 	var items = [];
-    while(i<5){
+    while(i<length){
         items.push(
             <Col xs="10" sm="4">
                 <Card
                     className="scrolling-card"
-                    style={{ maxWidth: '250px', maxHeight: '250px' }}
+                    style={{ maxWidth: '350px', maxHeight: '350px' }}
                     >
                     <CardBody
                         className="scrolling-card"
                         style={{ paddingTop: '10px' }}
                     >
+                        <h3 style={{textAlign: 'center'}}>{Object[i].association_name}</h3>
                         <CardImg top width="100%" src="https://dynomapper.com/images/Content_Audit_for_Website_Success.jpg"/>
-                        <p>Testing</p>
+                        <p style={{textAlign: 'center'}}>{Object[i].description}</p>
                     </CardBody>
                 </Card>
             </Col>
@@ -31,4 +35,38 @@ function cards(Object){
         </div>)
 }
 
-export default cards;
+function getProjects(Object){
+    console.log(Object)
+    console.log(Object.length)
+    var length = Object.length;
+    var i = 0;
+	var items = [];
+    while(i<length){
+        items.push(
+            <Col xs="10" sm="4">
+                <Card
+                    className="scrolling-card"
+                    style={{ maxWidth: '350px', maxHeight: '350px' }}
+                    >
+                    <CardBody
+                        className="scrolling-card"
+                        style={{ paddingTop: '10px' }}
+                    >
+                        <h3 style={{textAlign: 'center'}}>{Object[i].event_name}</h3>
+                        <CardImg top width="100%" src="https://dynomapper.com/images/Content_Audit_for_Website_Success.jpg"/>
+                        <p style={{textAlign: 'center'}}>{Object[i].description}</p>
+                        <p style={{textAlign: 'center'}}>{Object[i].activity_types}</p>
+                        <p style={{textAlign: 'center'}}>{Object[i].total_volunteers}</p>
+                    </CardBody>
+                </Card>
+            </Col>
+        );
+        i++;
+    }
+    return (
+		<div>
+			{items}
+        </div>)
+}
+
+export  {getAssociations,getProjects};
