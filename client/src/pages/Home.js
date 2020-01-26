705f95f270b09b1c9f83227c3876ad2964f7443d
steffan <steffan_v@hotmail.com>
2020-01-25 23:54:27 -0500

    links for projects

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,9 +1,12 @@
-import * as React from 'react';
+import React, { useState } from 'react';
 import Navbar from '../components/Navbar/Navbar';
 import {Nav} from 'react-bootstrap';
 import { Card, CardBody, Divider } from '../components/Card/Card';
 import {CardImg} from 'reactstrap';
+import Button from '@material-ui/core/Button';
+import CardActions from '@material-ui/core/CardActions';
 import ReactFullpage from '@fullpage/react-fullpage';
+import PropTypes from 'prop-types';
 import { Row, Col, Card as RBCard } from 'react-bootstrap/';
 import Subtitle from '../components/Subtitle/Subtitle';
 import Header from '../components/Header/Header';
@@ -16,9 +19,6 @@ function Home(){
     return(
         <div>
             <Header
-                title="Title 1"
-                title2="Title 2"
-                title3="Title 3"
             />
             <div className="background-section1">
                 <Subtitle text="Associations" />
@@ -31,6 +31,17 @@ function Home(){
                             className="scrolling-card"
                             style={{ maxWidth: '250px', maxHeight: '250px' }}
                             >
+                            <CardActions>
+                              <Button size="small" color="primary">
+                                Link 1
+                              </Button>
+                              <Button size="small" color="primary">
+                                Link 2
+                              </Button>
+                              <Button size="small" color="primary">
+                                Link 2
+                              </Button>
+                            </CardActions>
                             <CardBody
                                 className="scrolling-card"
                                 style={{ paddingTop: '10px' }}
